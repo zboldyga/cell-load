21e26a16c1fd041a6e2f69f09723e211791c70a2
Rishi Verma <rishioffic@gmail.com>
2025-07-21 16:33:52 -0700

    fix: fixed assignment of self.batches after create batches

--- src/cell_load/data_modules/samplers.py
+++ src/cell_load/data_modules/samplers.py
@@ -267,7 +267,7 @@ class PerturbationBatchSampler(Sampler):
     def __iter__(self) -> Iterator[list[int]]:
         # Shuffle the order of batches each time we iterate in non-distributed mode.
         if not self.distributed:
-            self._create_batches()
+            self.batches = self._create_batches()
         yield from self.batches
 
     def __len__(self) -> int:
@@ -283,7 +283,4 @@ class PerturbationBatchSampler(Sampler):
             epoch: Epoch number
         """
         self.epoch = epoch
-        
-        # Only shuffle in distributed mode.
-        if self.distributed:
-            self._create_batches()
+        self.batches = self._create_batches()
